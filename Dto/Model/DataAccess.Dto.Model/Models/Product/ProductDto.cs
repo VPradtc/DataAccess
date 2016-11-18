79c28d9d3a3c9063884a2c4217749c42f5c61325
Viacheslav Pradchuk <viacheslav.pradchuk@inoxoft.com>
2016-11-18 16:02:10 +0200

    wd

---
+++ Dto/Model/DataAccess.Dto.Model/Models/Product/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DataAccess.Dto.Model.Models.Base;
+using DataAccess.Entities.Entities;
+
+namespace DataAccess.Dto.Model.Models
+{
+    public class ProductDto : TrackableDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public override IProfileExpression Configure(IProfileExpression config)
+        {
+            config.CreateMap<Product, ProductDto>();
+            config.CreateMap<ProductDto, Product>();
+
+            return config;
+        }
+    }
+}
